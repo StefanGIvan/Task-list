6172d5fd10f7987e7fe981eb06a52d6a505dc468
Stefan Gabriel Ivan <stefanivan41@gmail.com>
2025-08-22 17:26:13 +0300

    Initial commit - task web app

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("form");
+const input = document.getElementById("task");
+const list = document.getElementById("list");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const text = input.value.trim();
+    if(text === "") return;
+
+    addTask(text);
+    input.value = "";
+});
+
+function addTask(text) {
+    const li = document.createElement("li");
+    li.classList.add("item");
+
+    const label = document.createElement("label");
+    label.classList.add("task");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("task-checkbox");
+
+    const spanText = document.createElement("span");
+    spanText.classList.add("task-title");
+    spanText.textContent = text;
+
+    label.appendChild(checkbox);
+    label.appendChild(span);
+
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("delete-btn");
+    delBtn.textContent = "X";
+
+    checkbox.addEventListener("change", () => {
+        li.classList.toggle("completed", checkbox.checked);
+    });
+
+    delBtn.addEleventListener("click", () =>{
+        li.remove();
+    });
+
+    li.appendChild(label);
+    li.appendChild(delBtn);
+    list.appendChild(li);
+}
