2b3d01b547b47fc17aa540b8a0c6b9de04b96799
Stefan Gabriel Ivan <stefanivan41@gmail.com>
2025-08-25 15:50:21 +0300

    Updated CSS for checkbox, ul, li and svg button, modified classes

--- script.js
+++ script.js
@@ -2,47 +2,47 @@ const form = document.getElementById("form");
 const input = document.getElementById("task");
 const list = document.getElementById("list");
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const text = input.value.trim();
-    if(text === "") return;
+  const text = input.value.trim();
+  if (text === "") return;
 
-    addTask(text);
-    input.value = "";
+  addTask(text);
+  input.value = "";
 });
 
 function addTask(text) {
-    const li = document.createElement("li");
-    li.classList.add("item");
+  const li = document.createElement("li");
+  li.classList.add("item");
 
-    const label = document.createElement("label");
-    label.classList.add("task");
+  const label = document.createElement("label");
+  label.classList.add("task");
 
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("task-checkbox");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("task-checkbox");
 
-    const spanText = document.createElement("span");
-    spanText.classList.add("task-title");
-    spanText.textContent = text;
+  const spanText = document.createElement("span");
+  spanText.classList.add("task-title");
+  spanText.textContent = text;
 
-    label.appendChild(checkbox);
-    label.appendChild(spanText);
+  label.appendChild(checkbox);
+  label.appendChild(spanText);
 
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("delete-btn");
-    delBtn.textContent = "X";
+  const delBtn = document.createElement("button");
+  delBtn.classList.add("delete-btn");
+  delBtn.innerHTML = `<img src="icons8-delete.svg" alt="Delete Button" class="delete-icon"/>`;
 
-    checkbox.addEventListener("change", () => {
-        li.classList.toggle("completed", checkbox.checked);
-    });
+  checkbox.addEventListener("change", () => {
+    li.classList.toggle("completed", checkbox.checked);
+  });
 
-    delBtn.addEventListener("click", () =>{
-        li.remove();
-    });
+  delBtn.addEventListener("click", () => {
+    li.remove();
+  });
 
-    li.appendChild(label);
-    li.appendChild(delBtn);
-    list.appendChild(li);
+  li.appendChild(label);
+  li.appendChild(delBtn);
+  list.appendChild(li);
 }
